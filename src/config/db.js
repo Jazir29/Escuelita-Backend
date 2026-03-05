28926e2b4a417e2d4566759d947ca1fee07c43aa
Jazir Olivera <jaziroliveram@gmail.com>
2026-03-05 11:18:34 -0500

    Configurar time zone-5

--- src/config/db.js
+++ src/config/db.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "orders_db",
+  timezone: '-05:00',   // ← Lima (UTC-5)
   ssl: {
     rejectUnauthorized: false
   },
